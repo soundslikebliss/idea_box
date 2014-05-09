cbc6b9f89056c846f61ff19fc7d0ce0a879558c3
soundslikebliss <russianid1@gmail.com>
2014-05-09 14:01:55 -0700

    adds idea save method and yaml store

--- app.rb
+++ app.rb
@@ -1,10 +1,25 @@
 class IdeaBoxApp < Sinatra::Base
+  require './idea'
 
   not_found do
     erb :error
   end
 
+
+
   get '/' do
     slim :index
   end
+
+  post '/' do
+  # 1. Create an idea based on the form parameters
+  # idea = Idea.new
+  idea = Idea.new(params['idea_title'], params['idea_description'])
+  # 2. Store it
+  idea.save
+  # 3. Send us back to the index page to see all ideas
+  redirect '/'
+
+  end
+
 end
